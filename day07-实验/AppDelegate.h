a8b4e399775f31cc32ba895f47feeac87588d2b9
1 <a1@1demacbook-pro-2.local>
2015-10-05 09:01:31 +0800

    Initial Commit

---
+++ day07-实验/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  day07-实验
+//
+//  Created by 1 on 15/10/5.
+//  Copyright © 2015年 sit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ day07-实验/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  day07-实验
+//
+//  Created by 1 on 15/10/5.
+//  Copyright © 2015年 sit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
