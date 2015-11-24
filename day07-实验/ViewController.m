1c0f1f503fd0a6c253b28b74804bfec90b3df564
stanJ <stan3081215@gmail.com>
2015-11-24 13:57:34 +0800

    first commit

--- day07-实验/ViewController.m
+++ day07-实验/ViewController.m
@@ -8,17 +8,68 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDataSource,UITableViewDelegate>
 
+@property (strong,nonatomic) NSMutableArray * fontsMutableArray;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self fontsArray];
+    NSLog(@"字体种类：%ld",self.fontsMutableArray.count);
+
+}
+#pragma mark-UITableViewDataSource && UITableViewDelegate
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.fontsMutableArray.count;
+}
+-(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:@"cell"];
+    }
+    NSString * str = [NSString stringWithFormat:@"helloWorld你好世界-%ld",indexPath.row];
+    //设置textLabel的文本的字体
+    cell.textLabel.attributedText = [[NSAttributedString alloc]initWithString:str attributes:[NSDictionary dictionaryWithObjectsAndKeys:[UIFont fontWithName:self.fontsMutableArray[indexPath.row] size:15],NSFontAttributeName,[UIColor redColor],NSForegroundColorAttributeName, nil]];
+    cell.detailTextLabel.text = self.fontsMutableArray[indexPath.row];
+    cell.detailTextLabel.font = [UIFont systemFontOfSize:10 weight:0.5];
+
+    return cell;
 }
 
+//得到所有字体名字的数组
+-(void)fontsArray{
+    self.fontsMutableArray = [NSMutableArray array];
+    NSArray * familyNamesArray = [NSArray arrayWithArray:[UIFont familyNames]];
+    for (NSString * familyName in familyNamesArray) {
+        NSArray * fontNamesArray = [NSArray arrayWithArray:[UIFont fontNamesForFamilyName:familyName]];
+        if (fontNamesArray.count == 0) {
+            [self.fontsMutableArray addObject:familyName];
+        }else {
+            for (NSString * fontName in fontNamesArray) {
+                [self.fontsMutableArray addObject:fontName];
+            }
+        }
+    }
+}
+
+
+
+/*
+//感叹号
+//    cell.accessoryType = UITableViewCellAccessoryDetailButton;
+//右尖角
+//    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+//感叹号+右尖角
+//    cell.accessoryType = UITableViewCellAccessoryDetailDisclosureButton;
+//对勾
+//    cell.accessoryType = UITableViewCellAccessoryCheckmark;
+*/
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
